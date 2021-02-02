ff78a94209bcabbbfdbe073b75edb2c4f7fbdbbb
Drake <apple@apples-MacBook-Pro.local>
2021-02-02 03:19:15 -0500

    cleaning code

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let cuzzo = document.querySelector(".cuzzo");
+let cuzzo = document.querySelector("#cuzzo");
 let bod = document.querySelector(".Bod");
 let begin = document.querySelector(".begin");
 let button = document.querySelector(".btn");
 let ul = document.querySelector(".ulTop");
 let bar = document.querySelector(".fa-bars");
+let body = document.querySelector("body");
 
 button.addEventListener("click", () => {
   bod.style.display = "block";
   begin.style.display = "none";
+  body.style.overflow = "visible";
 });
 let mobile = function () {
   if (begin.style.display === "none") {
@@ -16,6 +18,7 @@ let mobile = function () {
     cuzzo.addEventListener("animationend", () => {
       bod.style.display = "block";
       begin.style.display = "none";
+      body.style.overflow = "visible";
     });
   }
 };
