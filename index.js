3e039aab98a479ea2d7d2591ce1e2c1d51824c6a
Drake <apple@apples-MacBook-Pro.local>
2021-02-05 03:14:16 -0500

    first taste of backend

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 let PORT = process.env.PORT || 5000;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
+  alert("Working");
   res.sendFile(__dirname + "public/index.html");
+});
+app.post("/", function (req, res) {
+  console.log(req.body);
+});
+app.listen(PORT, () => {
   console.log("Working");
 });
-app.listen(PORT, () => console.log("Working"));
