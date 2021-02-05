ea3d0f350668e294da8cd171152e72b84be354c9
Drake <apple@apples-MacBook-Pro.local>
2021-02-05 03:32:08 -0500

    try again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let PORT = process.env.PORT || 5000;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   alert("Working");
-  res.sendFile(__dirname + "/publidsfsdfsc/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 app.post("/", function (req, res) {
   console.log(req.body.num1);
