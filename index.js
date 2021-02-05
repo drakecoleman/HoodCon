9c4b2be726ec09271126f1a5aaf6f46f6ead5d7d
Drake <apple@apples-MacBook-Pro.local>
2021-02-05 04:45:28 -0500

    test

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let PORT = process.env.PORT || 5000;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  console.log(req.body.num1);
   res.sendFile(__dirname + "public/index.html");
 });
 app.post("/", function (req, res) {
-  console.log(req.body.num1);
   res.send("Working");
 });
 app.listen(PORT, () => {
