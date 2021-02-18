dd69abcf9800303ce475fc85c2e7570f079da7d4
Drake <apple@apples-MacBook-Pro.local>
2021-02-18 11:50:28 -0500

    deploying

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const app = express();
-const sendMail = require("./form.js");
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 5000;
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "public/index.html");
-});
-app.post("/", function (req, res) {
-  // node form
-  res.send("Sent");
-});
-app.listen(PORT, () => {
-  console.log("Working");
-});
+// const express = require("express");
+// const app = express();
+// const sendMail = require("./form.js");
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: true }));
+// const PORT = process.env.PORT || 5000;
+// app.use(express.static("public"));
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "public/index.html");
+// });
+// app.post("/", function (req, res) {
+//   // node form
+//   res.send("Sent");
+// });
+// app.listen(PORT, () => {
+//   console.log("Working");
+// });
